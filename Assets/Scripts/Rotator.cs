0f076ff9d5859a496488263be1a624db35f8f4a6
Dilmer Valecillos <dilmervst@gmail.com>
2020-01-20 15:25:20 -0700

    Added changes for 2nd video

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed;
+
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime, Space.Self);
+    }
+}
