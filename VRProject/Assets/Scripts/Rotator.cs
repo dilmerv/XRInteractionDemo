4d7298744f5c01c53a6a8131fd9c940d9f9eea83
Dilmer Valecillos <dilmervst@gmail.com>
2020-01-28 07:07:30 -0700

    Added teleporting demo

---
+++ VRProject/Assets/Scripts/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed;
+
+    [SerializeField]
+    private bool rotate;
+
+    void Update()
+    {
+        if(rotate)
+            transform.Rotate(speed * Time.deltaTime, Space.World);
+    }
+
+    public void StartRotation()
+    {
+        rotate = true;
+    }
+
+    public void StopRotation()
+    {
+        rotate = false;
+    }
+}
